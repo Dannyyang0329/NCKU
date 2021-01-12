ec2d06d45e43778b85c874941d59c1eb9a2e18f5
Dannyyang0329 <f74094067@gs.nckku.edu.tw>
2021-01-13 00:33:45 +0800

    Final Exam

--- Final Exam/[25 Points] Find Maximum Fruit Number.c
+++ Final Exam/[25 Points] Find Maximum Fruit Number.c
@@ -1,17 +1,17 @@
 void find_fruit(char input[],char fruit[][15])
 {
-    int cnt[20]={0};
+    int cnt[20]={0};                                                        // cnt用來計每個水果數量 初始化為0(剛開始每個水果都0個)
 
-    char *s = strtok(input,"+_+");
+    char *s = strtok(input,"+_+");                                          // 初始化 這個s是第一個水果
     
-    while(s != NULL)
+    while(s != NULL)                                                        // 一直執行直到沒有水果
     {
-        for(int i=0;i<20;i++) if(strcmp(s,fruit[i]) == 0) cnt[i]++;
-        s = strtok(NULL,"+_+");
+        for(int i=0;i<20;i++) if(strcmp(s,fruit[i]) == 0) cnt[i]++;         // 判斷s是20個水果的哪一個 並且加1
+        s = strtok(NULL,"+_+");                                             // 找下一個水果 如果找不到會迴傳NULL
     }
 
-    int max=cnt[0];
-    for(int i=0;i<20;i++) max = (cnt[i]>max) ? cnt[i] : max;
+    int max=cnt[0];                                                         // 另一個新變數(最大值) 
+    for(int i=0;i<20;i++) max = (cnt[i]>max) ? cnt[i] : max;                // 找所有水果中數目中最多為多少
 
-    for(int i=0;i<20;i++) if(cnt[i] == max) printf("%s\n", fruit[i]);
+    for(int i=0;i<20;i++) if(cnt[i] == max) printf("%s\n", fruit[i]);       // 將數目最多的水果印出來
 }
